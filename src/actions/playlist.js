971d2f8d5770c993928a6583d055e9682ca3e2bf
Mickaël <mar@deezer.com>
2017-11-10 20:16:38 +0100

    Get random tracks from playlist

--- src/actions/playlist.js
+++ src/actions/playlist.js
@@ -9,10 +9,25 @@ export const changePlaylistId = id => dispatch => dispatch({
 	}
 })
 
+function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(0, size);
+}
+
 export const loadPlaylist = () => async (dispatch, getState) => {
-	const playlistId = getState().playlist.id
+    const state = getState()
+	const playlistId = state.playlist.id
+	const samplingCount = state.sampling.count
+    
 	jsonp(`https://api.deezer.com/playlist/${playlistId}?output=jsonp&strict=on`, null, function (err, data) {
-	 	const tracks = data.tracks.data
+        const tracks = getRandomSubarray(data.tracks.data,      samplingCount).map(item => item.preview)
+        
 	 	dispatch({
 			type: 'PLAYLIST_SET_TRACKS',
 			data: {
