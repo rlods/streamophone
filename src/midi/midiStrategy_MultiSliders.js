95d0abe0dfec9cabedb8d665cce53c0ad1a38308
Mickaël <mar@deezer.com>
2017-11-11 02:40:02 +0100

    bottom means no sound

--- src/midi/midiStrategy_MultiSliders.js
+++ src/midi/midiStrategy_MultiSliders.js
@@ -5,18 +5,25 @@ export default class MidiControllerMultilides {
 		this.slidersCount = 8 // TODO: configurable
 		this.slidersSteps = 128 // TODO: configurable
 		this.currentSamples = []
+		this.oldSample = -1
 		for (let i = 0; i < this.slidersCount; ++i)
 			this.currentSamples.push(0)
 	}
 
 	handleMessage(dispatch, ch, key, vel) {
 		const sample = (key * 8) - Math.floor(vel * 8 / this.slidersSteps) - 1
+		if (vel === 0) {
+			console.log("stop column")
+			dispatch(stopSample(this.oldSample))
+			this.currentSamples[key-1] = -1
+			return
+		}
 		if (ch === 176 && key >= 1 && key <= this.slidersCount) {
-			const oldSample = this.currentSamples[key-1]
-			if (sample !== oldSample) {
+			this.oldSample = this.currentSamples[key-1]
+			if (sample !== this.oldSample) {
 				// change sample
 				console.log("Start sample ", sample)
-				dispatch(stopSample(oldSample))
+				dispatch(stopSample(this.oldSample))
 				dispatch(startSample(sample))
 				this.currentSamples[key-1] = sample
 			}
