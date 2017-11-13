8535224624808a0ef36eb1180a679e0b6dd2013d
Mickaël <mar@deezer.com>
2017-11-13 15:27:21 +0100

    Added KORG NanoKey2

--- src/strategies/KORG_NanoKey2_Strategy.js
+++ src/strategies/KORG_NanoKey2_Strategy.js
@@ -1,15 +1,34 @@
-import { startSample, stopSample } from '../actions/sampling'
+import { startSample, stopSample, setSampleVolume } from '../actions/sampling'
 import Strategy from './Strategy'
 
 // --------------------------------------------------------------
 
-export default class KORG_NanoKey2_Strategy extends Strategy {
+const mapping = [42, 46]
+const keyDown = 144
+const keyUp = 128
+
+// --------------------------------------------------------------
+
+export default class KeyboardStrategy extends Strategy {
 	constructor() {
 		super()
 		this.samplesCount = 25
 	}
 
 	handleMIDI(dispatch, channel, key, velocity) {
-		// TODO
+		// the key is between 48 and 72
+		// channel 144 is key down and 128 keyup
+		// velocity works
+		if ((key >= 48 && key <= 72) || (mapping.findIndex(other => other === key)) > -1)  {
+            const sample = key-48
+            console.log(key, channel)
+            if (channel === keyDown) {
+                console.log("start sample", sample)
+                dispatch(startSample(sample))
+            } else if (channel === keyUp) {
+                dispatch(stopSample(sample))
+            }
+            dispatch(setSampleVolume(sample, velocity / 128))
+		}
 	}
 }
