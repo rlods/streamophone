150b59c37069a149db6ae948a358d51cd1e56087
Romain Lods <romain.lods@gmail.com>
2017-11-14 19:43:48 +0100

    Displaying freesound waveform

--- src/providers/FreesoundProvider.js
+++ src/providers/FreesoundProvider.js
@@ -20,7 +20,7 @@ export default class FreesoundProvider extends Provider
 
 	fetchAPI(url) {
 		return new Promise((resolve, reject) => {
-			jsonp(API_BASE_URL + url + `?format=jsonp&token=${config.TMP.FREESOUND}`, null, (err, data) => {
+			jsonp(API_BASE_URL + url + `&format=jsonp&token=${config.TMP.FREESOUND}`, null, (err, data) => {
 				if (err)
 					reject(err)
 				else if (data && data.error)
@@ -36,7 +36,20 @@ export default class FreesoundProvider extends Provider
 		switch (sourceType)
 		{
 			case 'freesound_pack':
-				tracks = (await this.fetchPackSounds(sourceId)).results.map(trackData => this.fetchTrack(trackData.id))
+				tracks = (await this.fetchPackSounds(sourceId)).results.map(track => {
+					const preview = track.previews ? track.previews['preview-lq-mp3'] : null
+					return {
+						cover: track.images.waveform_m,
+						id: track.id,
+						playing: false,
+						preview,
+						readable: !!preview,
+						title: track.name,
+						url: track.url,
+						volume1: 0.5,
+						volume2: 1.0
+					}
+				})
 				break
 			default:
 				throw new Error(`Unknown freesound source type "${sourceType}"`)
@@ -45,22 +58,6 @@ export default class FreesoundProvider extends Provider
 	}
 
 	async fetchPackSounds(packId) {
-		return this.fetchAPI(`packs/${packId}/sounds`)
-	}
-
-	async fetchTrack(trackId) {
-		const track = await this.fetchAPI(`sounds/${trackId}`)
-		const preview = track.previews ? track.previews['preview-lq-mp3'] : null
-		return {
-			// cover: ... no cover available,
-			id: track.id,
-			playing: false,
-			preview,
-			readable: !!preview,
-			title: track.name,
-			url: track.url,
-			volume1: 0.5,
-			volume2: 1.0
-		}
+		return this.fetchAPI(`packs/${packId}/sounds?fields=id,name,images,previews,url`)
 	}
 }
