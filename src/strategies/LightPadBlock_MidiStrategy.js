71061e3f4ff8fafe1cfa32dcad267878affb5c98
Romain Lods <romain.lods@gmail.com>
2017-11-17 00:22:10 +0100

    Simplify lightpadblock strategy

--- src/strategies/LightPadBlock_MidiStrategy.js
+++ src/strategies/LightPadBlock_MidiStrategy.js
@@ -4,46 +4,46 @@ import Strategy from './Strategy'
 // --------------------------------------------------------------
 
 const keyMapping = {
-    51: [0, 152, 136],
-    55: [1, 153, 137],
-    59: [2, 151, 135],
-    63: [3, 155, 139],
-    50: [4, 146, 130],
-    54: [5, 148, 132],
-    58: [6, 149, 133],
-    62: [7, 156, 140],
-    49: [8, 157, 141],
-    53: [9, 158, 142],
-    57: [10, 159, 143],
-    61: [11, 159, 143],
-    48: [12, 146, 130],
-    52: [13, 150, 134],
-    56: [14, 154, 138],
-    60: [15, 147, 131]
+	51:  0,
+	55:  1,
+	59:  2,
+	63:  3,
+	50:  4,
+	54:  5,
+	58:  6,
+	62:  7,
+	49:  8,
+	53:  9,
+	57: 10,
+	61: 11,
+	48: 12,
+	52: 13,
+	56: 14,
+	60: 15
 }
 
 // --------------------------------------------------------------
 
 export default class LightPadBlock_MidiStrategy extends Strategy {
-    constructor() {
-        super()
-        this.samplesCount = 16
-    }
+	constructor() {
+		super()
+		this.samplesCount = 16
+	}
 
 	handleMIDI(dispatch, channel, key, velocity) {
 		// messages are given four by four
 		if (key >= 48 && key <= 63 && channel < 209) {
-			console.log("key", key, "channel", channel)
-            const mapping = keyMapping[key]
-            if (mapping) {
-                console.log("mapping", mapping)
-                if (channel>=145 && channel<160) {
-                    dispatch(startSample(mapping[0]))
-                }
-                else if (channel<145 && channel>=129) {
-                    dispatch(stopSample(mapping[0]))
-                }
-            }
+			console.log("k", key, "c", channel, "v", velocity)
+			const mapping = keyMapping[key]
+			if (mapping !== undefined) {
+				console.log("m", mapping)
+				if (channel>=145 && channel<160) {
+					dispatch(startSample(mapping))
+				}
+				else if (channel<145 && channel>=129) {
+					dispatch(stopSample(mapping))
+				}
+			}
 		}
 	}
 }
