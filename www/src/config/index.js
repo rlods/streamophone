755c279b01d85a58f0c73376bcdaf169a27f0633
Romain Lods <romain.lods@gmail.com>
2017-11-26 14:37:11 +0100

    Curation

--- www/src/config/index.js
+++ www/src/config/index.js
@@ -22,6 +22,7 @@ const Config = {
 		{ sourceType: 'deezer_playlist', sourceId: '23804156',               title: 'Minimal electro house' },
 		{ sourceType: 'deezer_playlist', sourceId: '2762818384',             title: 'Musique concrète' },
 		{ sourceType: 'deezer_album',    sourceId: '13686698',               title: 'Mr. Robot, Vol. 1' },
+		{ sourceType: 'deezer_album',    sourceId: '12541456',               title: 'BNF Collection - Ella and Louis: The Anthology' },
 		{ sourceType: 'deezer_artist',   sourceId: '27',                     title: 'Daft Punk' },
 		{ sourceType: 'freesound_pack',  sourceId: '7447',                   title: 'Zeeland dunes' },
 		{ sourceType: 'freesound_pack',  sourceId: '16873',                  title: 'Industrial' },
